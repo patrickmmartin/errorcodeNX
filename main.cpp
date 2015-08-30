a3060cfc81c7de8da2084cf40d4dfaf2cdf8188c
patrickmmartin@gmail.com <patrickmmartin@gmail.com>
2015-08-30 19:06:12 +0100

    update with an implementation of required handler cases, with pass and fail

--- main.cpp
+++ main.cpp
@@ -18,11 +18,13 @@ class FooErrors
 public:
 	static const char EFOO[];
 	static const char EBAR[];
+	static const char EPOR[];
 };
 
 // this is a hand-crafted error definition
 const char FooErrors::EFOO[] = "GRP-FOO: Foo clobbered BAR on use";
 const char FooErrors::EBAR[] = SCOPE_ERROR("GRP", "FOO", "Foo not Bar");
+const char FooErrors::EPOR[] = SCOPE_ERROR("GRP", "FOO", "Foo not reparable");
 
 // we can define new unique instances
 typedef error_type<FooErrors::EFOO> foo_err;
@@ -161,11 +163,11 @@ void ErrorDispatcher<FooErrors::EFOO>::dispatchError()
 };
 
 
+// switch statements are basically just a problem -they mandate constants
+// so nothing here for a nice clean syntax of switch
+
 
 TEST_CASE( "test static dispatchers add case", "[errorcode]" ) {
-
-	// switch statements are basically just a problem -they mandate constants
-	// so nothing here for a nice clean syntax of switch
 
 	// However we can use template specialisation, as demonstrated below
 
@@ -180,65 +182,169 @@ TEST_CASE( "test static dispatchers add case", "[errorcode]" ) {
 
 }
 
+bool switch_default_pass_called = false;
+bool switch_default_fail_called = false;
+bool switch_foo_called = false;
 
-struct None { };
 
-template <typename H, typename T = None>
-struct typelist
+/**
+ * an error handler template, for later use
+ */
+template<error_code err_type>
+struct ErrorHandler;
+
+/** specialisation for FooErrors::EFOO
+ *
+ */
+template<>
+struct ErrorHandler<FooErrors::EFOO>
 {
-    typedef H Head;
-    typedef T Tail;
+    void operator()()
+    {
+  	    switch_foo_called = true;
+    }
 };
 
+/** specialisation for FooErrors::BAR
+ *
+ */
+template<>
+struct ErrorHandler<FooErrors::EBAR>
+{
+    void operator()()
+    {
+    }
+};
 
-
-// TODO(PMM) - next step is something that will achieve the same goals as a switch [shudder]
-
-
-struct NullType;
-
+/**
+ * typelist construction to support statically checked error handling
+ */
+struct PassType;
+struct FailType;
 template <error_code x, typename xs> struct ErrorList;
 
-template <typename T> struct OutputList;
+/** list checker template
+ *
+ */
+template <typename T> struct CheckList {};
 
-template <> struct OutputList<NullType> {
-  void operator()() {
-     std::cout << "Null" << std::endl;
+/**
+ * one base case for the list checker
+ */
+template <> struct CheckList<PassType> {
+  void operator()(error_code n, bool & handled) {
+	  switch_default_pass_called = true;
   }
 };
 
-template <int x, typename xs>
-   struct OutputList<ErrorList<x,xs> > {
-  void operator()() {
-    std::cout << x << ' ';
-    OutputList<xs>()();
+/**
+ * one base case for the list checker
+ */
+template <> struct CheckList<FailType> {
+  void operator()(error_code n, bool & handled) {
+	  switch_default_fail_called = true;
+  }
+};
+
+/**
+ * handler for the typelist of errors
+ */
+template <error_code x, typename xs>
+struct CheckList<ErrorList<x,xs> > {
+  void operator()(error_code n, bool & handled) {
+	  if (x == n)
+	  {
+		  handled = true;
+		  ErrorHandler<x>()();
+	  }
+	  if (!handled)
+	  {
+		  CheckList<xs>()(n, handled);
+	  }
+  }
+  // actual entry point
+  void operator()(error_code n)
+  {
+	  bool handled = false;
+	  operator()(n, handled);
   }
 };
 
 
-typedef ErrorList<FooErrors::EFOO,
-			ErrorList<FooErrors::EBAR, NullType> >
-    	DispatchList;
+TEST_CASE( "test list handlers with fallthrough pass", "[errorcode]" ) {
+
+	switch_default_pass_called = false;
+	switch_default_fail_called = false;
+	switch_foo_called = false;
+
+	/**
+	 * list to define a list with required handlers errors
+	 * passed in with fall-through
+	 * X, Y
+	 */
+	typedef ErrorList<FooErrors::EFOO,
+				ErrorList<FooErrors::EBAR,
+					PassType
+					> > ErrorsXY;
 
 
+	// exercising with constants
+    CheckList<ErrorsXY>()(FooErrors::EFOO);
+    CheckList<ErrorsXY>()(FooErrors::EBAR);
+    CheckList<ErrorsXY>()(FooErrors::EPOR);
 
+    // exercising with variables
+    error_code K = FooErrors::EFOO;
+    CheckList<ErrorsXY>()(K);
+    CHECK((switch_foo_called == true));
 
-TEST_CASE( "test dispatcher typelist", "[errorcode]" ) {
+    K = FooErrors::EBAR;
+    CheckList<ErrorsXY>()(K);
 
-
-//	DispatchList displist;
-
-	// TODO(PMM) - next step is something that will test an error_code *value* and match
-
-	error_code foo = FooErrors::EFOO;
-
-
-
-
-
-
-
+    K = FooErrors::EPOR;
+    CheckList<ErrorsXY>()(K);
+    CHECK((switch_default_pass_called == true));
 
 }
 
+
+TEST_CASE( "test list handlers with fallthrough fail", "[errorcode]" ) {
+
+	switch_default_pass_called = false;
+	switch_default_fail_called = false;
+	switch_foo_called = false;
+
+	/**
+	 * list to define a list with required handlers errors
+	 * passed and fall-through is an error
+	 * X, Y
+	 */
+	typedef ErrorList<FooErrors::EFOO,
+				ErrorList<FooErrors::EBAR,
+					FailType
+					> > ErrorsXYOnly;
+
+
+    // exercising with variables
+    error_code K = FooErrors::EFOO;
+
+    // exercising with variables
+    K = FooErrors::EFOO;
+    CheckList<ErrorsXYOnly>()(K);
+    CHECK((switch_foo_called == true));
+
+    K = FooErrors::EBAR;
+    CheckList<ErrorsXYOnly>()(K);
+
+    K = FooErrors::EPOR;
+    CheckList<ErrorsXYOnly>()(K);
+    CHECK((switch_default_fail_called == true));
+
+    // illustrates statically mandating handlers for specified error codes
+    // this cannot compile without an implementation of ErrorHandler<FooErrors::Z>
+    // error: invalid use of incomplete type 'struct ErrorHandler<((const char*)(& FooErrors::Z))>'
+//    K = FooErrors::Z;
+//    CheckList<ErrorsXYZ>()(K);
+
+}
 
