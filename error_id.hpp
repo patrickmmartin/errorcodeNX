43a99c68f35819e23870984207ca488f780be88b
Patrick Martin <patrickmmartin@gmail.com>
2016-07-16 16:15:19 +0100

    expanding on the explanation and fixing the SCOPE_ERROR_LOCATION macro

--- error_id.hpp
+++ error_id.hpp
@@ -8,16 +8,23 @@
 #ifndef ERROR_ID_HPP_
 #define ERROR_ID_HPP_
 
-#include <string>
-#include <iostream>
 #include <stdexcept>
 
-// ok, here's the big reveal...
-// the rvalue
+// error_id defines an array value, which must be initialised from a string literal
+// the _content_ of the error_id may or may not be unique, but following on from ODR
+// the pointer in a statically linked application must be unique,
+// whereas string constants may or not be folded if their content matches
+
+// the rvalue of an error_id is hence
 typedef char const error_id[];
 
-// and the lvalue
+// the lvalue for an error_id is hence
 typedef char const* error_value;
+
+// although the code will symbolically and semantically work perfectly with string literals that
+// [i] collide, [ii] are not human parseable [iii] not strings
+// it nevertheless would be a helpful affordance to generate unique readable strings
+
 
 /* Helper for concatenating argument in a standard
  * This is just a macro, and as such the semantics of the argument list
@@ -27,13 +34,16 @@ typedef char const* error_value;
  */
 #define SCOPE_ERROR(grp, pkg, error_str) grp "-" pkg ": " error_str
 
+// SCOPE_ERROR("GRP", "FOO", "Foo not Bar") == "GRP-FOO: Foo not Bar"
+
 // Organisations can exploit other preprocessor
 // features to ensure uniqueness of output
 
-#define TOSTRING(x) #x
+#define STRING(X) "" # X
+#define TOSTR(X) STRING(X)
 
 #define SCOPE_ERROR_LOCATION(grp, pkg, error_str) \
-        __FILE__ ":" TOSTRING(__LINE__) " " grp "-" pkg ": " error_str " "
+        __FILE__ ":" TOSTR(__LINE__) " " grp "-" pkg ": " error_str " "
 
 // we can define a simple template parameterised upon the error_id value
 template <error_id errtype> class typed_error_lite : public std::exception {};
